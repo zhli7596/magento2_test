0a50c2646aaa1d015d311c554e1a9788941d2d34
kokura_asah1 <zl2736@rit.edu>
2024-09-13 09:21:05 -0500

    idk

--- app/code/Magento/SendRequest/Controller/Adminhtml/sendrequest/index.php
+++ app/code/Magento/SendRequest/Controller/Adminhtml/sendrequest/index.php
@@ -1,44 +1,24 @@
 <?php
-
-namespace Magento\SendRequest\Controller\Adminhtml\sendrequest;
-
-use Magento\Backend\App\Action;
-use Magento\Framework\Controller\ResultFactory;
-use Magento\Framework\HTTP\Client\Curl;
-
-class SendRequest extends Action
+namespace Magento\SendRequest\Controller\Adminhtml\Sendrequest;
+class Index extends \Magento\Backend\App\Action
 {
-    protected $curl;
-
-    public function __construct(
-        Action\Context $context,
-        Curl $curl
-    ) {
-        $this->curl = $curl;
-        parent::__construct($context);
-    }
-
-    public function execute()
-    {
-        $url = 'http://example.com/';
-
-        $this->curl->get($url);
-
-        // You can also send POST request if needed:
-        // $this->curl->post($url, ['param1' => 'value1']);
-
-        // Get response
-        $response = $this->curl->getBody();
-
-        // Log or process the response as needed
-        $this->messageManager->addSuccessMessage(__('HTTP request sent successfully!'));
-
-        // Redirect back to the admin page
-        return $this->resultFactory->create(ResultFactory::TYPE_REDIRECT)->setPath('admin/dashboard/index');
-    }
-
-    protected function _isAllowed()
-    {
-        return $this->_authorization->isAllowed('Vendor_ModuleName::custom_menu');
-    }
+         protected $resultPageFactory = false;      
+         public function __construct(
+                 \Magento\Backend\App\Action\Context $context,
+                 \Magento\Framework\View\Result\PageFactory $resultPageFactory
+         ) {
+                 parent::__construct($context);
+                 $this->resultPageFactory = $resultPageFactory;
+         } 
+         public function execute()
+         {
+                 $resultPage = $this->resultPageFactory->create();
+                 $resultPage->setActiveMenu('Magento_SendRequest::menu');
+                 $resultPage->getConfig()->getTitle()->prepend(__('Demo Menu'));
+                 return $resultPage;
+         }
+         protected function _isAllowed()
+         {
+                 return $this->_authorization->isAllowed('Magento_SendRequest::menu');
+         }
 }
